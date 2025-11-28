bba1382bdde558058f7c3c8e8fd4edf4796b61b7
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2025-11-28 16:26:27 +0530

    sqlite

--- app/main.ts
+++ app/main.ts
@@ -2,20 +2,21 @@ import { open } from 'fs/promises';
 import { constants } from 'fs';
 
 const args = process.argv;
-const databaseFilePath: string = args[2]
+const databaseFilePath: string = args[2];
 const command: string = args[3];
 
 if (command === ".dbinfo") {
+    // Open the database file in read-only mode
     const databaseFileHandler = await open(databaseFilePath, constants.O_RDONLY);
+
+    // Read the first 100 bytes of the file (the database header)
+    // See: https://www.sqlite.org/fileformat.html#the_database_header
     const buffer: Uint8Array = new Uint8Array(100);
     await databaseFileHandler.read(buffer, 0, buffer.length, 0);
 
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    console.error("Logs from your program will appear here!");
-
-    // TODO: Uncomment the code below to pass the first stage    
-    // const pageSize = new DataView(buffer.buffer, 0, buffer.byteLength).getUint16(16);
-    // console.log(`database page size: ${pageSize}`);
+    // The page size is a 2-byte integer at offset 16 in the header
+    const pageSize = new DataView(buffer.buffer, 0, buffer.byteLength).getUint16(16);
+    console.log(`database page size: ${pageSize}`);
 
     await databaseFileHandler.close();
 } else {
